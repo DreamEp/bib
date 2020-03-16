4b97734bc778598dcfcb2f71321d7f9092b9473d
EtiennePinClaret <pinclaret@gmail.com>
2020-03-16 22:00:21 +0100

    Try to get json displayed in tab

--- server/website/src/PostMaitre.js
+++ server/website/src/PostMaitre.js
@@ -9,12 +9,11 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-const fs = require('fs');
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 200, align: 'right' },
-    { id: 'adress', label: 'Adresse', minWidth: 200, align: 'right' },
-    { id: 'phone', label: 'Phone\u00a0Number', minWidth: 200, align: 'right' },
+    { id: "name", label: 'Name', minWidth: 200, align: 'right' },
+    { id: "adress", label: 'Adress', minWidth: 200, align: 'right' },
+    { id: "phone", label: 'Phone\u00a0Number', minWidth: 200, align: 'right' },
 ];
 
 const useStyles = makeStyles({
@@ -26,11 +25,41 @@ const useStyles = makeStyles({
     },
 });
 
-const rows = [
-    MaitreData.map((details) => {
+function createData() {
+    MaitreData.map((details, index) => {
         return { name: details.name, adress: details.adress, phone: details.phone }
     })
+}
+
+
+/*
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}*/
+
+const rows = [
+    createData()
 ]
+
+/*
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];*/
 
 function PostMaitre() {
 
@@ -46,7 +75,7 @@ function PostMaitre() {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
-
+    console.log(rows);
     return (
         <Paper className={classes.root}>
             <TableContainer className={classes.container}>
@@ -67,7 +96,7 @@ function PostMaitre() {
                     <TableBody>
                         {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
                             return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.phone}>
                                     {columns.map(column => {
                                         const value = row[column.id];
                                         return (
