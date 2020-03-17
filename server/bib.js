1e1fd4981617ed6f07511c92a2719898970ba9b9
EtiennePinClaret <pinclaret@gmail.com>
2020-03-17 13:40:28 +0100

    Changed named to fill the workshop

--- server/bib.js
+++ server/bib.js
@@ -6,13 +6,13 @@ var maitreJson = fs.readFileSync("Maitre.json");
 var michelinRest = JSON.parse(michelinJson);
 var maitreRest = JSON.parse(maitreJson);
 
-var both = [];
+var bib = [];
 
 michelinRest.forEach(michelin => {	
 	maitreRest.forEach(maitre => {
 		if(maitre.phone === michelin.phone){
-            both.push(JSON.stringify(michelin, null, 2));
-            console.log("Adding new restaurant for the Both.json file : ");	
+            bib.push(JSON.stringify(michelin, null, 2));
+            console.log("Adding new restaurant for the bib.json file : ");	
             console.log(michelin);
 	    }	
 	});
@@ -22,9 +22,9 @@ console.log("Number of michelin restaurant at the begining :");
 console.log(michelinRest.length);
 console.log("Number of maitre restaurant at the begining :");
 console.log(maitreRest.length);
-console.log("Number of restaurant wich phone number match both michelin and maitre :");
-console.log(both.length);
+console.log("Number of restaurant wich phone number match bib michelin and maitre :");
+console.log(bib.length);
 
-fs.appendFileSync('Both.json',"[ \n");
-fs.appendFileSync('Both.json',both);
-fs.appendFileSync('Both.json',"\n]");
+fs.appendFileSync('Bib.json',"[ \n");
+fs.appendFileSync('Bib.json',bib);
+fs.appendFileSync('Bib.json',"\n]");
